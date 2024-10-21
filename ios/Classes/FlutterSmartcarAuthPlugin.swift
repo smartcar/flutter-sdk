a66057ffe16778c7933d05f2361a304571f31cec
Jesus Coronado <jesus.coronado@geekbears.com>
2024-10-21 12:05:17 -0700

    build!: upgraded ios smartcar sdk & minimum OS version
    We have upgraded the **SmartcarAuth** sdk version to `6.0.2`.
    
    **Now the minimum OS version for iOS is `13.0`**

--- ios/Classes/FlutterSmartcarAuthPlugin.swift
+++ ios/Classes/FlutterSmartcarAuthPlugin.swift
@@ -4,142 +4,138 @@ import SmartcarAuth
 
 public class FlutterSmartcarAuthPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
     
-  private var eventSink: FlutterEventSink?
-
-  /// SmartcarAuth instance
-  private var smartcarAuth: SmartcarAuth?
-
-
-  /// FlutterPlugin
+    private var eventSink: FlutterEventSink?
     
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let methodChannel = FlutterMethodChannel(name: "smartcar/flutter_smartcar_auth", binaryMessenger: registrar.messenger())
-    let eventChannel = FlutterEventChannel(name: "smartcar/flutter_smartcar_auth/events", binaryMessenger: registrar.messenger())
-    let instance: FlutterSmartcarAuthPlugin = FlutterSmartcarAuthPlugin()
-    eventChannel.setStreamHandler(instance)
-    registrar.addMethodCallDelegate(instance, channel: methodChannel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-      case "setup":
-        self.setup(arguments: call.arguments as! Dictionary<String, Any?>, result: result)
-        break
-      case "launchAuthFlow":
-        self.launchAuthFlow(arguments: call.arguments as! Dictionary<String, Any?>, result: result)
-        break
-      default:
-        result(FlutterMethodNotImplemented)
-        break
+    /// SmartcarAuth instance
+    private var smartcarAuth: SmartcarAuth?
+    
+    /// SmartcarAuth url builder dictionary
+    private let authUrlActions: [String : (_ urlBuilder: SCUrlBuilder, _ value: Any?) -> Void] = [
+        "forcePrompt": { urlBuilder, value in
+            let _ = urlBuilder.setForcePrompt(forcePrompt: value as! Bool)
+        },
+        "singleSelect": { urlBuilder, value in
+            let _ = urlBuilder.setSingleSelect(singleSelect: value as! Bool)
+        },
+        "flags": { urlBuilder, value in
+            let _ = urlBuilder.setFlags(flags: value as! [String])
+        },
+        "state": { urlBuilder, value in
+            let _ = urlBuilder.setState(state: value as! String)
+        },
+        "make": { urlBuilder, value in
+            let _ = urlBuilder.setMakeBypass(make: value as! String)
+        },
+        "vin": { urlBuilder, value in
+            let _ = urlBuilder.setSingleSelectVin(vin: value as! String)
+        },
+        "user": { urlBuilder, value in
+            let _ = urlBuilder.setUser(user: value as! String)
+        },
+    ]
+    
+    
+    /// FlutterPlugin
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let methodChannel = FlutterMethodChannel(name: "smartcar/flutter_smartcar_auth", binaryMessenger: registrar.messenger())
+        let eventChannel = FlutterEventChannel(name: "smartcar/flutter_smartcar_auth/events", binaryMessenger: registrar.messenger())
+        let instance: FlutterSmartcarAuthPlugin = FlutterSmartcarAuthPlugin()
+        eventChannel.setStreamHandler(instance)
+        registrar.addMethodCallDelegate(instance, channel: methodChannel)
     }
-  }
-
-  /// FlutterStreamHandler
-
-  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-    self.eventSink = events
-    return nil
-  }
     
-  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    self.eventSink = nil
-    return nil
-  }
-
-  /// FlutterSmartcarAuthPlugin methods used through MethodChannel
-
-  private func setup(arguments: Dictionary<String, Any?>, result: FlutterResult) -> Void {
-    do {
-      try self.smartcarAuth = SmartcarAuth(
-        clientId: arguments["clientId"] as! String,
-        redirectUri: arguments["redirectUri"] as! String,
-        scope: arguments["scopes"] as! Array<String>,
-        completionHandler: responseHandler,
-        testMode: arguments["testMode"] as! Bool
-      )
-
-      result(nil)
-    } catch {
-      result(
-        FlutterError(
-          code: "SETUP_SMARTCAR_ERROR",
-          message: "\(error)",
-          details: nil
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "setup":
+            self.setup(arguments: call.arguments as! Dictionary<String, Any?>, result: result)
+            break
+        case "launchAuthFlow":
+            self.launchAuthFlow(arguments: call.arguments as! Dictionary<String, Any?>, result: result)
+            break
+        default:
+            result(FlutterMethodNotImplemented)
+            break
+        }
+    }
+    
+    /// FlutterStreamHandler
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        return nil
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil
+    }
+    
+    /// FlutterSmartcarAuthPlugin methods used through MethodChannel
+    private func setup(arguments: Dictionary<String, Any?>, result: FlutterResult) -> Void {
+        self.smartcarAuth = SmartcarAuth(
+            clientId: arguments["clientId"] as! String,
+            redirectUri: arguments["redirectUri"] as! String,
+            scope: arguments["scopes"] as! Array<String>,
+            completionHandler: responseHandler,
+            mode: SCMode(rawValue: arguments["mode"] as! String)
         )
-      )
-    }
-  }
-
-  private func launchAuthFlow(arguments: Dictionary<String, Any?>, result: FlutterResult) -> Void {
-    do {
-      if (self.smartcarAuth != nil) {
-        let authUrl = self.smartcarAuth!.authUrlBuilder()
-
-        if (arguments.keys.contains("forcePrompt")) {
-          authUrl.setForcePrompt(forcePrompt: arguments["forcePrompt"] as! Bool)
-        }
-
-        if (arguments.keys.contains("singleSelect")) {
-          authUrl.setSingleSelect(singleSelect: arguments["singleSelect"] as! Bool)
-        }
-
-        if (arguments.keys.contains("flags") && arguments["flags"] != nil) {
-          authUrl.setFlags(flags: arguments["flags"] as! [String])
-        }
-
-        if (arguments["state"] != nil && arguments["state"] is String) {
-          authUrl.setState(state: arguments["state"] as! String)
-        }
-
-        if (arguments["make"] != nil && arguments["make"] is String) {
-          authUrl.setMakeBypass(make: arguments["make"] as! String)
-        }
-
-        if (arguments["vin"] != nil && arguments["vin"] is String) {
-          authUrl.setSingleSelectVin(vin: arguments["vin"] as! String)
-        }
-
-        let url = authUrl.build()
-
-        try self.smartcarAuth!.launchAuthFlow(url: url)
-
+        
         result(nil)
-      } else {
-        result(
-          FlutterError(
-            code: "LAUNCH_AUTH_FLOW_ERROR",
-            message: "SmartcarAuth is not configured yet, please call Smartcar.setup() first.",
-            details: nil
-          )
-        )
-      }
-    } catch {
-      result(
-        FlutterError(
-          code: "LAUNCH_AUTH_FLOW_ERROR",
-          message: "\(error)",
-          details: nil
-        )
-      )
     }
-
-  }
-
-  /// Method to handle the SmartcarAuth response
-
-  private func responseHandler(code: String?, state: String?, virtualKeyUrl: String? ,error: AuthorizationError?) -> Void {
-    do {
-      if (eventSink != nil) {
-        let data: [String : Any?] = [
-          "code": code,
-          "state": state,
-          "virtualKeyUrl":virtualKeyUrl,
-          "error": error?.description,
-        ]
-        try eventSink!(data)
-      }
-    } catch {
-      
+    
+    private func launchAuthFlow(arguments: Dictionary<String, Any?>, result: FlutterResult) -> Void {
+        do {
+            if (self.smartcarAuth != nil) {
+                var authUrl = self.smartcarAuth!.authUrlBuilder()
+                
+                arguments.forEach { body in
+                    authUrlActions[body.key]?(authUrl, body.value)
+                }
+                
+                let url = authUrl.build()
+                
+                self.smartcarAuth!.launchAuthFlow(
+                    url: url,
+                    viewController: UIApplication.shared.delegate!.window!!.rootViewController!
+                )
+                
+                result(nil)
+            } else {
+                result(
+                    FlutterError(
+                        code: "LAUNCH_AUTH_FLOW_ERROR",
+                        message: "SmartcarAuth is not configured yet, please call Smartcar.setup() first.",
+                        details: nil
+                    )
+                )
+            }
+        }
     }
-  }
+    
+    /// Method to handle the SmartcarAuth response
+    private func responseHandler(
+        code: String?,
+        state: String?,
+        virtualKeyUrl: String?,
+        error: AuthorizationError?
+    ) -> Void {
+        if (self.eventSink != nil) {
+            var data: [String : Any?]
+            
+            if (error == nil) {
+                data = [
+                    "code": code,
+                    "state": state,
+                    "virtualKeyUrl": virtualKeyUrl,
+                ]
+            } else {
+                data = [
+                    "type": error!.type,
+                    "description": error!.errorDescription,
+                ]
+            }
+            
+            self.eventSink!(data)
+        }
+        
+    }
 }
